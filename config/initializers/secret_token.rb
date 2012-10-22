091a7c049a443d83c217e283ee18a0b7b1706acc
Geoff Crew <gc97@cornell.edu>
2012-10-22 12:44:45 -0400

    initial rails-3.2.8 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvweb::Application.config.secret_token = 'bc1561c8ad6d8a0d0dc2b2cfa81aeb4ddcee3c42a3f3015a231dd5c06ac867bab811e59d98f316c7cd470ebffb13b3eb39c4e0ff6a5f7ce2130a17bc59a4c694'

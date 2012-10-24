3182adc85b127ab330af445b18cdc8482c9bf24d
Geoff Crew <gc97@cornell.edu>
2012-10-24 09:56:26 -0400

    put associations in model between characters and members

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ActiveRecord::Base
   attr_accessible :blm, :blu, :brd, :bst, :cor, :dnc, :drg, :drk, :mnk, :name, :nin, :pld, :pup, :rdm, :remarks, :rng, :sam, :sch, :smn, :thf, :war, :whm
+  belongs_to :member
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ActiveRecord::Base
   attr_accessible :active, :admin, :cycle_date, :join_date, :name, :password, :remarks, :reward_cycle, :url
+  has_many :characters
 end

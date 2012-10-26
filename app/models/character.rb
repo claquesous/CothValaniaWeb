ae6e6539857bc5916f68382d0839bb2c27e1fdc7
Geoff Crew <gc97@cornell.edu>
2012-10-26 01:31:56 -0400

    fix characters/members association

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,5 @@
 class Character < ActiveRecord::Base
   attr_accessible :blm, :blu, :brd, :bst, :cor, :dnc, :drg, :drk, :mnk, :name, :nin, :pld, :pup, :rdm, :remarks, :rng, :sam, :sch, :smn, :thf, :war, :whm
-  validates_presence_of :name
   belongs_to :member
-  validates_associated :member
+  validates_presence_of :name, :member
 end

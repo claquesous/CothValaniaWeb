b2d229f46189a8b8ae72600a405b5801529dcc6c
Geoff Crew <gc97@cornell.edu>
2012-10-27 18:31:49 -0400

    remove attr_protected in favor of attr_accessible

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,6 @@
 class Member < ActiveRecord::Base
   attr_accessible :active, :name, :password, :remarks, :url
   has_secure_password
-  attr_protected :admin, :cycle_date, :reward_cycle, :join_date
   validates_presence_of :name, :password, :on => :create
   has_many :characters
 end

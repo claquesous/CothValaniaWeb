bdde93fe73ff553ac16994181839728d1192dc5e
Geoff Crew <gc97@cornell.edu>
2012-11-01 17:45:08 -0400

    show character name on event_attendances

--- spec/views/event_attendances/index.html.haml_spec.rb
+++ spec/views/event_attendances/index.html.haml_spec.rb
@@ -5,11 +5,11 @@ describe "event_attendances/index" do
     assign(:event_attendances, [
       stub_model(EventAttendance,
         :event_occurrence => nil,
-        :character => nil
+        :character => mock_model("Character", :name => "Character")
       ),
       stub_model(EventAttendance,
         :event_occurrence => nil,
-        :character => nil
+        :character => mock_model("Character", :name => "Character")
       )
     ])
   end
@@ -17,6 +17,7 @@ describe "event_attendances/index" do
   it "renders a list of event_attendances" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 4
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => "Character".to_s, :count => 2
   end
 end

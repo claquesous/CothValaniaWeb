1bb15ac24a7430bd3316cf6861303bbd91e02654
Geoff Crew <gc97@cornell.edu>
2012-11-03 22:16:31 -0400

    guarantee unique race names

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,6 +1,7 @@
 class Race < ActiveRecord::Base
   attr_accessible :name, :remarks
   has_many :characters
+  validates_uniqueness_of :name
 
   def name=(s)
     write_attribute(:name, s.to_s.titleize)

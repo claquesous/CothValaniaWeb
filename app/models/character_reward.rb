e89a0fecb59dce3afe6c237211b5ae6d3aeb15a3
Geoff Crew <gc97@cornell.edu>
2012-11-17 13:32:45 -0500

    flip-flop uniqueness and scope checker for better error messages

--- app/models/character_reward.rb
+++ app/models/character_reward.rb
@@ -4,6 +4,6 @@ class CharacterReward < ActiveRecord::Base
   belongs_to :reward
   belongs_to :occurrence
   attr_accessible :obtained, :obtained_points, :preference, :remarks, :reward_cycle, :reward, :character
-  validates_uniqueness_of :member_id, :scope => [:preference]
+  validates_uniqueness_of :preference, :scope => :member_id
   validates_associated :member, :reward
 end

3f1d8451d6d5724ac7f4853a58c7194e37bcc207
Geoff Crew <gc97@cornell.edu>
2012-11-25 14:21:29 -0500

    only show last 10 shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,7 +3,7 @@ class ShoutsController < ApplicationController
   # GET /shouts
   # GET /shouts.json
   def index
-    @shouts = Shout.all
+    @shouts = Shout.limit(10)
 
     respond_to do |format|
       format.html # index.html.erb

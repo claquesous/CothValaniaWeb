aabdde16d0190eab168992a4c0da36f38203b0ad
Geoff Crew <gc97@cornell.edu>
2012-11-26 19:08:50 -0500

    spell out variables and use find instead of select...first

--- app/helpers/occurrences_helper.rb
+++ app/helpers/occurrences_helper.rb
@@ -1,8 +1,8 @@
 module OccurrencesHelper
-  def get_requirements(r,c)
-    requirements = r.select{|s| (s.used_occurrence == @occurrence) && s.character == c}.first
+  def get_requirements(requirement,character)
+    requirements = requirement.find{|s| (s.used_occurrence == @occurrence) && s.character == character}
     if requirements.nil?
-      r.select{|s| s.used_occurrence.nil? && s.character == c}.first
+      requirement.find{|s| s.used_occurrence.nil? && s.character == character}
     else
       requirements
     end

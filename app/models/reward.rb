3886fc6df0c16857978e4a1f250ef89894d46e02
Geoff Crew <gc97@cornell.edu>
2012-11-27 23:48:48 -0500

    set inverse for event_rewards

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,7 +2,7 @@ class Reward < ActiveRecord::Base
   default_scope order(:name)
   attr_accessible :external_value, :name, :remarks, :event_rewards_attributes
   validates_presence_of :name
-  has_many :event_rewards
+  has_many :event_rewards, :inverse_of => :reward
   has_many :events, :through => :event_rewards
   has_many :character_rewards
   validates_uniqueness_of :name

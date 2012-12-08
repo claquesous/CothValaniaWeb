f5b18db6ab96a39d0232441b86669a8dcf25e498
Geoff Crew <gc97@cornell.edu>
2012-12-08 00:24:21 -0500

    create method for unobtained_or_occurrence

--- app/models/character_reward.rb
+++ app/models/character_reward.rb
@@ -1,6 +1,6 @@
 class CharacterReward < ActiveRecord::Base
   scope :unobtained, where("obtained=? or obtained is null", false)
-  scope :obtained, where("obtained=?",true)
+  scope :obtained, where(obtained: true)
   scope :active, joins(:member).where("members.active")
   belongs_to :member
   belongs_to :character
@@ -9,4 +9,8 @@ class CharacterReward < ActiveRecord::Base
   attr_accessible :obtained, :obtained_points, :preference, :remarks, :reward_cycle, :reward, :character
   validates_uniqueness_of :preference, :scope => [:member_id, :reward_cycle], :allow_nil => true
   validates_presence_of :member, :reward
+
+  def self.unobtained_or_occurrence(occurrence)
+    active.unobtained || obtained.where(occurrence_id: occurrence.id)
+  end
 end
--- spec/models/character_reward_spec.rb
+++ spec/models/character_reward_spec.rb
@@ -2,6 +2,32 @@ require 'spec_helper'
 
 describe CharacterReward do
   it { should belong_to(:character) }
+  it { should belong_to(:member) }
   it { should belong_to(:reward) }
   it { should belong_to(:occurrence) }
+
+  describe "unobtained_or_occurrence" do
+    before :each do
+      ActiveModel::MassAssignmentSecurity::WhiteList.any_instance.stub(:deny?).and_return(false)
+      @occurrence = mock_model("Occurrence")
+    end
+
+    it "should return something received in the occurrence" do
+      occurrence = FactoryGirl.create(:occurrence)
+      cr = CharacterReward.create!(reward: mock_model("Reward"), member: mock_model("Member"), character: mock_model("Character"), occurrence: occurrence)
+      CharacterReward.all.should eq([cr])
+    end
+
+    it "should return something on a wishlist for someone who is active" do
+      member = FactoryGirl.create(:member, active: true)
+      cr = CharacterReward.create!(reward: mock_model("Reward"), member: member, character: mock_model("Character"), occurrence: @occurrence)
+      CharacterReward.active.should eq([cr])
+    end
+
+    it "should not return something on a wishlist for someone who is not active" do
+      member = FactoryGirl.create(:member, active: false)
+      cr = CharacterReward.create!(reward: mock_model("Reward"), member: member, character: mock_model("Character"), occurrence: @occurrence)
+      CharacterReward.unobtained_or_occurrence(@occurrence).should eq([])
+    end
+  end
 end

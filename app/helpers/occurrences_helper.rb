56010a1969748f83f6cbedc16c8de15a66f55ba3
Geoff Crew <gc97@cornell.edu>
2012-12-08 14:21:07 -0500

    combine if/else conditions

--- app/helpers/occurrences_helper.rb
+++ app/helpers/occurrences_helper.rb
@@ -1,10 +1,6 @@
 module OccurrencesHelper
   def get_requirements(requirement,character)
     requirements = requirement.find{|s| (s.used_occurrence == @occurrence) && s.character == character}
-    if requirements.nil?
-      requirement.find{|s| s.used_occurrence.nil? && s.character == character}
-    else
-      requirements
-    end
+    requirements ||= requirement.find{|s| s.used_occurrence.nil? && s.character == character}
   end
 end

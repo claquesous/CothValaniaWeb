9ac1a7736b4bc9404f8763e268d4be1b8be9524f
Geoff Crew <gc97@cornell.edu>
2012-12-11 01:28:25 -0500

    event_attendance has one member

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -4,6 +4,7 @@ class EventAttendance < ActiveRecord::Base
   belongs_to :occurrence
   belongs_to :character
   has_one :event, :through => :occurrence
+  has_one :member, through: :character
   validates_presence_of :character, :occurrence
   validates_uniqueness_of :character_id, :scope => :occurrence_id
 

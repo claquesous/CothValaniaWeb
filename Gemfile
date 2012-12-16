a8bbf3403dba032b8db41d4521663c4cde56df44
Geoff Crew <gc97@cornell.edu>
2012-12-16 11:51:57 -0500

    add bullet profiling and fix a few n+1 queries

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ group :test do
   gem 'simplecov', :require => false
 end
 
+group :development do
+  gem 'bullet'
+end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ Cvweb::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = false
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.disable_browser_cache = true
+  end
 end

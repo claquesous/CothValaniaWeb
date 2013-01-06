288e2594ee52f76973db56304ff932d4327017be
Geoff Crew <gc97@cornell.edu>
2013-01-06 17:09:16 -0500

    incorporate bootstrap and update styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
     field = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize, :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(field)}\")")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(field)}\")", class: "btn btn-success")
   end
 end

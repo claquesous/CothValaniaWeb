80f9d58dcfcb2b0eb3dbb1000d50b3489b696cbd
Geoff Crew <gc97@cornell.edu>
2013-11-29 05:34:32 +0000

    fix visible scope

--- app/models/event_reward.rb
+++ app/models/event_reward.rb
@@ -3,7 +3,7 @@ class EventReward < ActiveRecord::Base
   belongs_to :reward
   validates_presence_of :event, :reward
   attr_accessible :event_id
-  scope :visible, joins(:event).where{(events.hidden == true) | (events.hidden == nil) }
+  scope :visible, joins(:event).where{(events.hidden == false) | (events.hidden == nil) }
 
   def drop_rate
     reward.character_rewards.obtained_from(event).count.to_f / event.occurrences.count
